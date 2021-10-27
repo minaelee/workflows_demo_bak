2629ddbf2fc84d4bfac7ac29ab7fb8faa69f7763
minaelee <minae.lee@gmail.com>
2021-10-27 16:44:57 -0700

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def main():
+    print("Hello World!" + str(sys.version_info))
+    if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
+        # let's make this script fail for python 3.6
+        x = 1/0
+        
+        
+if __name__ == "__main__":
+    main()
