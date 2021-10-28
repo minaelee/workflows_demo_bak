89c1ae27ac6eb9472e44398bc093de22dd1c4efe
minaelee <minae.lee@gmail.com>
2021-10-27 17:08:08 -0700

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -2,9 +2,10 @@ import sys
 
 
 def main():
-    print("Hello World! " + str(sys.version_info))
-    #if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
-    raise Exception("This app is not compatible with Python 3.6.")
+    print("Hello World! From Python: " + str(sys.version_info))
+    if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
+        # let's make this script fail for Python 3.6
+        raise Exception('Python version 3.6.x is unsupported!')
         
         
 if __name__ == "__main__":
